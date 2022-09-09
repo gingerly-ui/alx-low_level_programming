65694dc8a0933e329a104f2c46851b6a45cc24f9
gingerly-ui <111126422+gingerly-ui@users.noreply.github.com>
2022-09-09 19:03:34 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 /**
  * main- print the string in the put function
  * this program will print 'Programming is like building a multilingual puzzle'*/
- return: 0
 int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
