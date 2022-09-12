815243ba5272cc604e9abd9f9ad64efdf68b79e0
gingerly-ui <111126422+gingerly-ui@users.noreply.github.com>
2022-09-12 17:32:59 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
- #include <stdio.h>
+#include <stdio.h>
 
 /**
- *main - print the string in the put function
- *
- *description: using the main function
- *this program prints "programming is like building a multilingual puzzle 
- *return 0;
- */
-int main(void)	
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
 {
-	puts("\"Programming_is_like_building_a_multilingual_puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
