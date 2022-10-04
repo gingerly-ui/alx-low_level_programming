21c2739560916dbd1641ed7f2178140b176c8e23
gingerly-ui <kholisilesimnikiwe@gmail.com>
2022-10-04 12:50:25 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,4 +12,13 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
+}
 
